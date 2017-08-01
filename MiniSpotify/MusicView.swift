c5a30b5c876a85028ea33ada5c10f10b518c0feb
Farbod909 <rafezyfarbod@gmail.com>
2017-08-01 01:36:04 -0700

    Display song info and toggle play/pause

--- MiniSpotify/MusicView.swift
+++ MiniSpotify/MusicView.swift
@@ -10,8 +10,52 @@ import Cocoa
 
 class MusicView: NSView {
     @IBOutlet weak var albumCover: NSImageView!
+    @IBOutlet weak var songName: NSTextField!
+    @IBOutlet weak var artistName: NSTextField!
     @IBOutlet weak var playPauseButton: NSButton!
+
+    var isPlaying = false
+
+    func setIsPlayingState(state: String) {
+        if state == "playing" {
+            isPlaying = true
+            playPauseButton.image = #imageLiteral(resourceName: "pauseImage")
+        } else {
+            isPlaying = false
+            playPauseButton.image = #imageLiteral(resourceName: "playImage")
+        }
+    }
+
+    func pause() {
+        playPauseButton.image = #imageLiteral(resourceName: "playImage")
+
+        let script =    "tell application \"Spotify\"\n" +
+                            "pause\n" +
+                        "end tell"
+        var error: NSDictionary?
+        NSAppleScript(source: script)?.executeAndReturnError(&error)
+    }
+
+    func play() {
+        playPauseButton.image = #imageLiteral(resourceName: "pauseImage")
+
+        let script =    "tell application \"Spotify\"\n" +
+                            "play\n" +
+                        "end tell"
+        var error: NSDictionary?
+        NSAppleScript(source: script)?.executeAndReturnError(&error)
+    }
+
+
     @IBAction func quitApplication(_ sender: NSButton) {
         NSApplication.shared().terminate(self)
+    }
+    @IBAction func playPauseToggle(_ sender: NSButton) {
+        if isPlaying {
+            pause()
+        } else {
+            play()
+        }
+        isPlaying = !isPlaying
     }
 }
--- MiniSpotify/StatusMenuController.swift
+++ MiniSpotify/StatusMenuController.swift
@@ -26,5 +26,61 @@ class StatusMenuController: NSObject {
         musicMenuItem = statusMenu.item(withTitle: "Music")
         musicMenuItem.view = musicView
 
+        musicView.songName.stringValue = getCurrentSongName()
+        musicView.artistName.stringValue = getCurrentArtist()
+        musicView.setIsPlayingState(state: getPlayerState())
     }
+
+    func getCurrentSongName() -> String {
+        var songName = ""
+        let script =    "set track to getCurrentTrackName()\n" +
+                        "on getCurrentTrackName()\n" +
+                            "tell application \"Spotify\"\n" +
+                                "set currentTrackName to name of current track as string\n" +
+                                "return currentTrackName\n" +
+                            "end tell\n" +
+                        "end getCurrentTrackName"
+        var error: NSDictionary?
+        if let scriptObject = NSAppleScript(source: script) {
+            let output: NSAppleEventDescriptor = scriptObject.executeAndReturnError(&error)
+            songName = output.stringValue ?? "No Song Playing"
+        }
+        return songName
+    }
+
+    func getCurrentArtist() -> String {
+        var artist = ""
+        let script =    "set artist to getCurrentArtist()\n" +
+                        "on getCurrentArtist()\n" +
+                            "tell application \"Spotify\"\n" +
+                                "set currentArtist to artist of current track as string\n" +
+                                "return currentArtist\n" +
+                            "end tell\n" +
+                        "end getCurrentArtist"
+        var error: NSDictionary?
+        if let scriptObject = NSAppleScript(source: script) {
+            let output: NSAppleEventDescriptor = scriptObject.executeAndReturnError(&error)
+            artist = output.stringValue ?? "N/A"
+        }
+        return artist
+    }
+
+    func getPlayerState() -> String {
+        var state = ""
+        let script =    "set state to getCurrentState()\n" +
+                        "on getCurrentState()\n" +
+                            "tell application \"Spotify\"\n" +
+                                "set currentState to player state as string\n" +
+                                "return currentState\n" +
+                            "end tell\n" +
+                        "end getCurrentState"
+        var error: NSDictionary?
+        if let scriptObject = NSAppleScript(source: script) {
+            let output: NSAppleEventDescriptor = scriptObject.executeAndReturnError(&error)
+            state = output.stringValue ?? "N/A"
+        }
+        return state
+
+    }
+
 }
